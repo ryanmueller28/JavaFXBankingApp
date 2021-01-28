b965f1344553f34ea275a336f165a6623b9fb54a
ryamnmueller28 <ryan.mueller@doane.edu>
2021-01-28 16:27:31 -0600

    UPdated

--- src/Controller/SQLCustomerController.java
+++ src/Controller/SQLCustomerController.java
@@ -6,7 +6,7 @@ import java.sql.*;
 public class SQLCustomerController {
     private static String jbdcSchema = "jdbc:mysql://192.168.0.68:3306/?user=ryan";
     private static String sqlUsrName = "ryan";
-    private static String sqlUsrPwd = "M4tr1mC4uth0n";
+    private static String sqlUsrPwd = "";
     private static String databaseName = "DevCustomerDatabase";
 
     Connection connection = DriverManager.getConnection(jbdcSchema, sqlUsrName, sqlUsrName);
