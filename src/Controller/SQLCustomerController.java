256077072b6c744ff4a124bed2401b7dfe6e2db6
ryamnmueller28 <ryan.mueller@doane.edu>
2021-01-28 19:09:08 -0600

    Added SQL functions for creating the Customer DB.

--- src/Controller/SQLCustomerController.java
+++ src/Controller/SQLCustomerController.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import Model.Customer;
+
 import java.sql.*;
 
 
@@ -10,9 +12,7 @@ public class SQLCustomerController {
     private static String databaseName = "DevCustomerDatabase";
 
     Connection connection = DriverManager.getConnection(jbdcSchema, sqlUsrName, sqlUsrName);
-
-    // Customer object
-
+    Statement statement = null;
     // Customer Array
 
 /*
@@ -23,6 +23,74 @@ public class SQLCustomerController {
      * @throws SQLException
      */
     public SQLCustomerController() throws SQLException {
+
+    }
+
+    public void createCustomerDB(){
+        String createTableCustomer = "CREATE DATABASE BankInfo";
+        try{
+            statement = connection.createStatement();
+            statement.execute(createTableCustomer);
+        } catch (SQLException se) {
+            se.printStackTrace();;
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if (statement != null){
+                    statement.close();
+                }
+            }catch (SQLException se2){
+
+            }try{
+                if (connection != null){
+                    connection.close();
+                }
+            }catch (SQLException se3){
+                se3.printStackTrace();
+            }
+        }
+
+        createCustomerTable();
+    }
+
+    public void createCustomerTable(){
+        String SQLCreateCustomerTable = "CREATE TABLE CUSTOMER (" +
+                "ID int, " +
+                "FirstName varchar(255)," +
+                "LastName varchar(255)," +
+                "Address varchar(255)," +
+                "PhoneNumber BIGINT," +
+                "CheckingAccountNum BIGINT," +
+                "SavingsAccountNum BIGINT)";
+        try{
+            statement = connection.createStatement();
+            statement.execute(SQLCreateCustomerTable);
+        } catch (SQLException se) {
+            se.printStackTrace();;
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if (statement != null){
+                    statement.close();
+                }
+            }catch (SQLException se2){
+
+            }try{
+                if (connection != null){
+                    connection.close();
+                }
+            }catch (SQLException se3){
+                se3.printStackTrace();
+            }
+        }
+
+    }
+
+    public void addCustomerToDB(Customer customerToAdd){
 
     }
 
